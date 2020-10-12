392682ec4fcd83768572869241e36de8a43964de
Damiansalguero <damian.salguero@aikq.de>
2020-10-12 19:42:45 +0200

    add server + routes + landing

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 ///////////////////// ROUTES /////////////////////////
 app.get("/", function(req, res) {
   res.send("WORKING!!!!!!!!!!");
+});
+
+app.get("/landing", function(req, res) {
+  res.render("landing");
 });
 ///////////////////// ROUTES /////////////////////////
 
