c98a37f9d23b885f5508217a2a043e2fc3dc1cad
Damiansalguero <damian.salguero@aikq.de>
2020-11-09 14:05:55 +0100

    update app.js npm + dependency setup

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//////////////// USE ///////////////////
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 ///////////////////// ROUTES /////////////////////////
 app.get("/", function(req, res) {
   res.send("WORKING!!!!!!!!!!");
