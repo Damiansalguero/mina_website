c2f6f14b1874e7aa7f260987a5e5e70a56f2f84b
Damiansalguero <damian.salguero@aikq.de>
2020-12-05 19:02:22 +0100

    show + user routes working & login + reg working + navbar working

--- routes/shows.js
+++ routes/shows.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const shows = require("../controllers/shows");
 
 router.get("/", shows.renderLanding);
+router.get("/workshops", shows.renderWorkshops);
+router.get("/partizipatives-projekt", shows.renderPartizip);
+router.get("/prozess-begleitung", shows.renderProzess);
+router.get("/kontakt", shows.renderKontakt);
 
 module.exports = router;
