c7766aad8f1674bf87f79180afbf57e5605ebe90
Damiansalguero <damian.salguero@aikq.de>
2020-12-29 13:45:13 +0100

    add scroll up button to boilerplate

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,5 +1,4 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("scrollBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
