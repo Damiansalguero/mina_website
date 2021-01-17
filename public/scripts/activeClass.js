100be016a65796566aa199e2d08eb41e19b10af6
Damiansalguero <damian.salguero@aikq.de>
2021-01-17 11:47:46 +0100

    change active class script navbar

--- public/scripts/activeClass.js
+++ public/scripts/activeClass.js
@@ -1,25 +1,10 @@
-function setActive() {
-//   aObj = document
-//     .getElementById("navbarNavAltMarkup")
-//     .getElementsByClassName("nav-link");
-//   for (i = 0; i < aObj.length; i++) {
-//     if (document.location.href.indexOf(aObj[i].href) >= 0) {
-//       aObj[i].className = "nav-link active";
-//     }
-//   }
-// }
-//
-// window.onload = setActive;
-
 $(function() {
   var current = location.pathname;
-  console.log(location.pathname);
   $(".navbar-nav .nav-item a").each(function() {
     var $this = $(this);
-    // if the current path is like this link, make it active
     if ($this.attr("href").indexOf(current) !== -1) {
       $this.addClass("active");
-    } else if ($this.attr("href").indexOf(current) === "/mina") {
+    } else if ($this.attr("href").indexOf(current) === "/mina/home") {
       $this.addClass("active");
     }
   });
