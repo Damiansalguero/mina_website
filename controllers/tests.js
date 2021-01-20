b020407cd8335581c277e25d86ebea432ed1ec0d
Damiansalguero <damian.salguero@aikq.de>
2021-01-20 10:33:18 +0100

    change test flash messages

--- controllers/tests.js
+++ controllers/tests.js
@@ -24,7 +24,7 @@ module.exports.createTest = async (req, res, next) => {
   await test.save();
 
   //Flash message needs to be specified and declared here + Setup in app.js (middleware in app.use)
-  req.flash("success", "Der Post wurde erfolgreich erstellt !");
+  req.flash("success", "Der Test wurde erfolgreich erstellt !");
   // res.redirect(`/posts/${post._id}`);
   res.redirect("/test");
 };
@@ -32,7 +32,7 @@ module.exports.createTest = async (req, res, next) => {
 module.exports.showTest = async (req, res) => {
   const test = await Test.findById(req.params.id);
   if (!test) {
-    req.flash("error", "Dieser Post existiert nicht mehr !");
+    req.flash("error", "Dieser Test existiert nicht mehr !");
     return res.redirect("/mina/home");
   }
   res.render("tests/show", { test });
@@ -41,7 +41,7 @@ module.exports.showTest = async (req, res) => {
 module.exports.renderEditForm = async (req, res) => {
   const { id } = req.params;
   const test = await Test.findById(req.params.id);
-  res.render("testedit", { test });
+  res.render("tests/edit", { test });
 };
 
 module.exports.updatetest = async (req, res) => {
@@ -49,7 +49,7 @@ module.exports.updatetest = async (req, res) => {
   const test = await Test.findByIdAndUpdate(id, {
     ...req.body.test
   });
-  req.flash("success", "Successfully updated campground");
+  req.flash("success", "Test wurde erfolgreich aktualisiert");
   res.redirect(`/test`);
 };
 
