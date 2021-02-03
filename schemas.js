f2d10d7d77a06a4941635fc7146ee8849269e3d8
Damiansalguero <damian.salguero@aikq.de>
2021-02-03 18:59:59 +0100

    add wsregister model + schema + middleware

--- schemas.js
+++ schemas.js
@@ -46,11 +46,31 @@ module.exports.workshopGallerySchema = Joi.object({
 });
 
 module.exports.workshopregisterSchema = Joi.object({
-  wsg: Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required()
-  }).required(),
-  deleteImages: Joi.array()
+  wsr: Joi.object({
+    location: Joi.string().required(),
+    date: Joi.string().required(),
+    name: Joi.string().required(),
+    organisation: Joi.string().required(),
+    membership: Joi.array().required(),
+    street: Joi.string().required(),
+    zip: Joi.string().required(),
+    phone: Joi.string().required(),
+    assistance: Joi.array().required(),
+    assistanceneed: Joi.string().required(),
+    aid: Joi.array().required(),
+    aids: Joi.string().required(),
+    care: Joi.array().required(),
+    caretaker: Joi.array().required(),
+    volunteer: Joi.array().required(),
+    allergy: Joi.array(),
+    allergies: Joi.string(),
+    dietwish: Joi.string(),
+    orgafield: Joi.array().required(),
+    description: Joi.string().required(),
+    wishes: Joi.string().required(),
+    personal: Joi.array().required(),
+    newsletter: Joi.string().optional()
+  }).required()
 });
 
 module.exports.timelineSchema = Joi.object({
