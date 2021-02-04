4456ac1da9d72afe493f21c25683ad1198e3a30f
Damiansalguero <damian.salguero@aikq.de>
2021-02-04 09:22:48 +0100

    fix timeline prozess

--- controllers/wsregistrations.js
+++ controllers/wsregistrations.js
@@ -1,6 +1,8 @@
 const Anmeldung = require("../models/wsregistration");
+const Workshop = require("../models/workshop");
 const { workshopregisterSchema } = require("../schemas.js");
 
-module.exports.renderAnmeldung = (req, res) => {
-  res.render("wsregister/register");
+module.exports.renderAnmeldung = async (req, res) => {
+  const workshops = await Workshop.find({});
+  res.render("wsregister/register", { workshops });
 };
