8e4c66d523d8114ae6d48c80a4837d3c06b84ed6
Damiansalguero <damian.salguero@aikq.de>
2021-03-17 11:03:41 +0100

    add bib files + change style prozess + adapt modals in prozess

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 //"mongodb://localhost:27017/mina"
 //process.env.DB_URL ||
-// const dbUrl = "mongodb://localhost:27017/mina";
+// const dbUrl = process.env.DB_URL;
 const dbUrl = "mongodb://localhost:27017/mina";
 const MongoStore = require("connect-mongo")(session);
 //////////////// MODEL IMPORT //////////////////
