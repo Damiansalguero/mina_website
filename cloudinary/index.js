aed33c0ca2c57cf20dbaa3f01a8585d041d02865
Damiansalguero <damian.salguero@aikq.de>
2021-04-21 12:39:44 +0200

    add changes to bib styles

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -4,7 +4,7 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
@@ -25,12 +25,12 @@ const storage = new CloudinaryStorage({
       "mp4",
       "mpeg",
       "pdf",
-      "webm"
-    ]
-  }
+      "webm",
+    ],
+  },
 });
 
 module.exports = {
   cloudinary,
-  storage
+  storage,
 };
