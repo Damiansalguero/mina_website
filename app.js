1519d71e2426ef047bca0c9bd36ad73260f1cc34
Damiansalguero <damian.salguero@aikq.de>
2021-05-06 09:24:07 +0200

    fix prozess img show page

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const LocalStrategy = require("passport-local");
 //"mongodb://localhost:27017/mina"
 //process.env.DB_URL ||
 // const dbUrl = process.env.DB_URL;
-const dbUrl = process.env.DB_URL;
+const dbUrl = "mongodb://localhost:27017/mina";
 const MongoStore = require("connect-mongo")(session);
 //////////////// MODEL IMPORT //////////////////
 const User = require("./models/user");
